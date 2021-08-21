44e587a8554cc951ae8473f5e466f998a2e8034b
namnv <hiepnq_frl@beetsoft.com.vn>
2021-08-21 18:50:44 +0700

    page top

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'lib/js/jquery.min'
+import 'lib/js/bootstrap.min'
+import 'lib/js/popper.min'
 import "@fortawesome/fontawesome-free/css/all"
+
 
 Rails.start()
 Turbolinks.start()
